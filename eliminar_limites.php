a232af0a3151eb6e493af0ab85af268ab013ebe0
isiscarolina-coder <isiscarolina@gmail.com>
2026-03-10 15:28:58 -0400

    Update eliminar_limites.php

--- eliminar_limites.php
+++ eliminar_limites.php
@@ -1,6 +1,5 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
-header("Content-Type: application/json; charset=UTF-8");
 
 $host = "gateway01.us-east-1.prod.aws.tidbcloud.com";
 $user = "4Asq3bxQtZ3iP3r.root";
@@ -11,6 +10,12 @@ $port = 4000;
 $conexion = mysqli_init();
 $ca_cert = "/etc/ssl/certs/ca-certificates.crt"; 
 mysqli_ssl_set($conexion, NULL, NULL, $ca_cert, NULL, NULL);
+
+$resultado = @mysqli_real_connect($conexion, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
+
+if (!$resultado) {
+    die(json_encode(["status" => "error", "message" => "Fallo conexión: " . mysqli_connect_error()]));
+}
 
 $idAdmin = isset($_GET['idAdmin']) ? intval($_GET['idAdmin']) : 0;
 
